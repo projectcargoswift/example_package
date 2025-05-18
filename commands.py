9c12505e43534b73a26de06e6914b81bde6aeca4
projectcargoswift <main.cargoswift@gmail.com>
2025-05-18 18:08:14 +0200

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,3 +1,9 @@
-def greet(name):  
+# files/packages/example_package/commands.py  
+
+def greet(user_name):  
     """Vytiskne pozdrav uživateli."""  
-    print(f"Hello, {name}! Welcome to the example package.")  
+    print(f"Hello, {user_name}! Welcome to the example package.")  
+
+# A registrace pro dynamické načítání příkazů  
+def register_commands(command_registry):  
+    command_registry["greet user"] = greet  
